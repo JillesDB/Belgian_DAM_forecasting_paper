1f84a222c960d2c13468b5679b663bcd460b4316
JillesDB <141919440+JillesDB@users.noreply.github.com>
2023-12-13 11:23:46 +0100

    Update Code 13 Dec
    Update Code 13 Dec

--- Cleaned_code/Plotting/Heatmap_coefficient_plotting.py
+++ Cleaned_code/Plotting/Heatmap_coefficient_plotting.py
@@ -12,7 +12,8 @@ path_datasets_folder = str(Path.cwd().parent) + '\Dataframes_with_coefficients'
 path_forecasts_folder = str(Path.cwd().parent) + '\Forecasts_for_plots'
 path_real_prices = os.path.join(path_datasets_folder,'Real_prices.csv')
 
-def plot_coefficient_matrix_heatmap(name_csv,calibration_window,begin_time=None,end_time=None):
+def plot_coefficient_matrix_heatmap(name_csv,calibration_window,start=None,end=None,
+                                    from_hour = '00:00',to_hour='23:00'):
     """
 
     Parameters
@@ -25,28 +26,48 @@ def plot_coefficient_matrix_heatmap(name_csv,calibration_window,begin_time=None,
     """
     path_file = os.path.join(path_datasets_folder,name_csv)
     dataframe_coefficients = pd.read_csv(path_file)
-    if begin_time != None:
-        day_selection = (dataframe_coefficients['datetime'] >= begin_time) & (dataframe_coefficients['datetime'] <= end_time)# + datetime.timedelta(hours=23))
+    if start != None:
+        day_selection = (dataframe_coefficients['datetime'] >= start) & (dataframe_coefficients['datetime'] <= end)# + datetime.timedelta(hours=23))
         filtered_dataframe = dataframe_coefficients.loc[day_selection]
     else:
         filtered_dataframe = dataframe_coefficients
     print(filtered_dataframe)
     filtered_dataframe = filtered_dataframe.set_index(pd.DatetimeIndex(filtered_dataframe['datetime'])).drop(columns=['Unnamed: 0','datetime'])
-    filtered_dataframe_day = filtered_dataframe.between_time('06:30','22:30') #16 hours per day
-    filtered_dataframe_night = filtered_dataframe.between_time('22:30','06:30')#8 hours per night
+    filtered_dataframe = filtered_dataframe.between_time(from_hour,to_hour) #16 hours per day
+    # filtered_dataframe_night = filtered_dataframe.between_time('22:30','06:30')#8 hours per night
     # print(filtered_dataframe_day,filtered_dataframe,filtered_dataframe_night)
     # filtered_dataframe = filtered_dataframe.apply(lambda y: (y - y.mean()) / y.std(), axis=0)
     # filtered_dataframe_day = filtered_dataframe_day.apply(lambda y: (y - y.mean()) / y.std(), axis=0)
     # filtered_dataframe_night = filtered_dataframe_night.apply(lambda y: (y - y.mean()) / y.std(), axis=0)
-    nb_row = 16*7*4 #15*7 hours per week => four week average
-    filtered_dataframe_day = filtered_dataframe_day.rolling(nb_row).mean()[nb_row-1:]
+    nb_hours = datetime.datetime.strptime(to_hour, '%H:%M') - datetime.datetime.strptime(from_hour, '%H:%M')
+    print(nb_hours)
+    nb_row = int((nb_hours.seconds+3600)/3600*7*4) #15*7 hours per week => four week average
+    # Normalize the data
     filtered_dataframe = filtered_dataframe.rolling(nb_row).mean()[nb_row-1:]
-    filtered_dataframe_night = filtered_dataframe_night.rolling(nb_row).mean()[nb_row-1:]
-    plt.figure(figsize=(6, 5))
-    ax = sns.heatmap(filtered_dataframe_day,yticklabels=nb_row)# yticklabels = 8760/nb_row
+    # filtered_dataframe_night = filtered_dataframe_night.rolling(nb_row).mean()[nb_row-1:]
+    plt.figure(figsize=(12,8))
+    ax = sns.heatmap(filtered_dataframe,yticklabels=nb_row*2,cmap='RdYlGn',vmin=-0.10,vmax = 0.10)# yticklabels = 8760/nb_row
     ax.set_title('Coefficients for CW' + str(calibration_window)+' from '+str(filtered_dataframe.index[0]) + ' until ' +str(filtered_dataframe.index[-1]))
     plt.tight_layout()
     plt.show()
 
-plot_coefficient_matrix_heatmap(name_csv='Aggregated_Coefficients_Full_Dataset_CW56.csv',calibration_window=56,
-                                begin_time='2020-01-01 00:00:00',end_time='2021-01-01 23:00:00')
+
+# df = pd.read_csv(r'C:\Users\jdeblauw\Documents\GitHub\Belgian_DAM_forecasting_paper\Cleaned_code\Dataframes_with_Coefficients\Raw_coefficientsFull_Dataset_CW112.csv')
+# print(df)
+# df = df.drop(columns=['Unnamed: 0','Unnamed: 0.1'])
+# #df = df.reset_index().rename(columns={df.index.name:'Unnamed: 0'})
+# # # df.index.name = 'datetime'
+# df.to_csv(r'C:\Users\jdeblauw\Documents\GitHub\Belgian_DAM_forecasting_paper\Cleaned_code\Dataframes_with_Coefficients\Raw_coefficientsFull_Dataset_CW112.csv',mode = 'w')
+# #
+#
+# df = pd.read_csv(r'C:\Users\jdeblauw\Documents\GitHub\Belgian_DAM_forecasting_paper\Cleaned_code\Dataframes_with_Coefficients\Raw_coefficientsFull_Dataset_CW84.csv')
+# df = df.drop(columns=['index'])
+# df.to_csv(r'C:\Users\jdeblauw\Documents\GitHub\Belgian_DAM_forecasting_paper\Cleaned_code\Dataframes_with_Coefficients\Raw_coefficientsFull_Dataset_CW84.csv',mode = 'w')
+# df = pd.read_csv(r'C:\Users\jdeblauw\Documents\GitHub\Belgian_DAM_forecasting_paper\Cleaned_code\Dataframes_with_Coefficients\Raw_coefficientsFull_Dataset_CW112.csv')
+# df = df.drop(columns=['index'])
+# df.to_csv(r'C:\Users\jdeblauw\Documents\GitHub\Belgian_DAM_forecasting_paper\Cleaned_code\Dataframes_with_Coefficients\Raw_coefficientsFull_Dataset_CW112.csv',mode = 'w')
+
+plot_coefficient_matrix_heatmap(name_csv='Raw_coefficientsFull_Dataset_CW56.csv',calibration_window=56,
+                               start='2020-01-01 00:00:00',end='2022-12-31 23:00:00'
+                                # ,from_hour='11:30',to_hour='12:30'
+                                )
