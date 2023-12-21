5673ebcdc0cb13e7de6285dd4a2d8abb3b500cc0
JillesDB <141919440+JillesDB@users.noreply.github.com>
2023-12-21 23:03:17 +0100

    Update Coefficients_Bar_Chart_Plot.py

--- Cleaned_code/Plotting/Coefficients_Bar_Chart_Plot.py
+++ Cleaned_code/Plotting/Coefficients_Bar_Chart_Plot.py
@@ -8,7 +8,7 @@ alt.data_transformers.disable_max_rows()
 
 import numpy as np
 
-cwd = Path.cwd()
+cwd = Path.cwd().parent
 path_datasets_folder = os.path.join(cwd,'Datasets')
 path_coeff_folder = os.path.join(cwd,'Dataframes_with_Coefficients')
 path_real_prices = pd.read_csv(os.path.join(path_datasets_folder,'Real_prices.csv'))
