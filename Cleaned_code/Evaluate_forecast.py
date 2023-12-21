ed418760f0cb9921b889430602162c960ae7823b
JillesDB <141919440+JillesDB@users.noreply.github.com>
2023-12-21 13:41:03 +0100

    Update Evaluate_forecast.py

--- Cleaned_code/Evaluate_forecast.py
+++ Cleaned_code/Evaluate_forecast.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 cwd = Path.cwd()
 path_forecasts_folder = os.path.join(cwd,'Forecasts')
-path_datasets_folder = os.path.join(cwd,'\Datasets')
+path_datasets_folder = os.path.join(cwd,'Datasets')
 path_real_prices = os.path.join(path_datasets_folder,'Real_prices.csv')
 
 
